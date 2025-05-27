05f122969dbc8c9fb681d5624b8589a702ab2a8e
Angie <angiepolitte@gmail.com>
2025-05-27 15:50:59 -0500

    validations in React

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,42 @@ import "./App.css";
 import { useForm } from "react-hook-form";
 
 function App() {
-  const { register, handleSubmit, watch } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const onSubmit = (data) => console.log(data);
-
-  // console.log(watch("name"));
-  // console.log(watch("email"));
-
-  const watchedName = watch("name");
-  const watchedEmail = watch("email");
-
-  useEffect(() => {
-    console.log("Name ", watchedName);
-  }, [watchedName]);
-
-  useEffect(() => {
-    console.log("Email ", watchedEmail);
-  }, [watchedEmail]);
 
   return (
     <div>
       <h1>Forms in React</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>
-          Name: <input {...register("name")} />
+          Name:{" "}
+          {/* <input {...register("name", { required: true, minLength: 2 })} /> */}
+          <input
+            {...register("name", {
+              required: "Name is required",
+              minLength: {
+                value: 2,
+                message: "Name should be at least 2 chars",
+              },
+            })}
+          />
         </label>
+        {errors.name && <p>{errors.name.message}</p>}
+
         <label>
           Email:
-          <input {...register("email")} />
+          <input
+            {...register("email", {
+              required: true,
+              pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            })}
+          />
         </label>
+        {errors.email && <p>Email is required</p>}
         <button type="submit">Submit</button>
       </form>
     </div>
